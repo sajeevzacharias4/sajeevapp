525bf289bcc5b3b8ac284be24445398736f84789
sajeevzacharias4 <sajeev.zacharias4@gmail.com>
2013-01-02 11:34:26 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sajeevapp::Application.config.secret_token = 'f8ac6992db60098f4da73e91f697bc98cb24b20ba521d2bd290cd3f8aa27c05181030774a8c080710c413a2b0ecf1460da9c1f8d6b5203ef28b54efbfcd2210f'
